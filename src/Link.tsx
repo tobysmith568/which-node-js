3512854b951bc5b89feb9503682f85aba774de02
Toby Smith <tobysmith568@hotmail.co.uk>
2022-06-25 21:35:40 +0100

    Add the third-party page

--- src/Link.tsx
+++ src/Link.tsx
@@ -5,15 +5,20 @@ import { FC, PropsWithChildren, useMemo } from "react";
 interface Props {
   href: string;
   newTab?: boolean;
+  color?: string;
+  hoverColor?: string;
 }
 
-const Link: FC<PropsWithChildren<Props>> = ({ children, href, newTab }) => {
+const Link: FC<PropsWithChildren<Props>> = ({ children, href, newTab, color, hoverColor }) => {
+  color = color ?? "#ccc";
+  hoverColor = hoverColor ?? "#fff";
+
   const target = useMemo(() => (newTab ? "_blank" : undefined), [newTab]);
   const rel = useMemo(() => (href.startsWith("http") ? "noopener noreferrer" : undefined), [href]);
 
   return (
     <NextLink href={href} passHref>
-      <MenuAnchor target={target} rel={rel}>
+      <MenuAnchor target={target} rel={rel} color={color} hoverColor={hoverColor}>
         {children}
       </MenuAnchor>
     </NextLink>
@@ -21,12 +26,17 @@ const Link: FC<PropsWithChildren<Props>> = ({ children, href, newTab }) => {
 };
 export default Link;
 
-const MenuAnchor = styled.a`
-  color: #ccc;
+interface MenuAnchorProps {
+  color: string;
+  hoverColor: string;
+}
+
+const MenuAnchor = styled.a<MenuAnchorProps>`
+  color: ${props => props.color};
   text-decoration: none;
 
   &:hover {
     text-decoration: underline;
-    color: #fff;
+    color: ${props => props.hoverColor};
   }
 `;
---
+++ src/pages/third-party.tsx
@@ -0,0 +1,92 @@
+import styled from "@emotion/styled";
+import { getProjectLicenses } from "generate-license-file";
+import { ILicense } from "generate-license-file/dist/models/license";
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import { FC, Fragment } from "react";
+import BackHomeLink from "../BackHomeLink";
+import Link from "../Link";
+
+interface Props {
+  licenses: ILicense[];
+}
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const licenses = await getProjectLicenses("./package.json");
+
+  return {
+    props: { licenses: licenses.map(l => ({ content: l.content, dependencies: l.dependencies })) }
+  };
+};
+
+const LicensesPage: FC<Props> = ({ licenses }) => {
+  return (
+    <>
+      <Head>
+        <title>Licenses</title>
+        <meta name="description" content="Third-party licenses" />
+        <meta name="robots" content="noindex" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <PageWrapper>
+        <BackHomeLink />
+
+        <h1>Third-Party Licenses</h1>
+        <h3>This page lists all of the third-party licenses used by this website</h3>
+        <h4>
+          This page was generated using the{" "}
+          <Link
+            href="https://www.npmjs.com/package/generate-license-file"
+            newTab
+            color="#000"
+            hoverColor="#000">
+            generate-license-file npm package
+          </Link>
+          .
+        </h4>
+        {licenses.map((license, i) => getLicense(license, i))}
+      </PageWrapper>
+    </>
+  );
+};
+export default LicensesPage;
+
+const PageWrapper = styled.div`
+  max-width: 800px;
+  margin: 3em auto 3em auto;
+`;
+
+const getLicense = (license: ILicense, i: number) => {
+  return (
+    <Fragment key={i}>
+      <p>The following npm packages may be included in this website:</p>
+      <ul>
+        {license.dependencies.map(dependency => (
+          <li key={dependency}>
+            <Link href={getPackageUrl(dependency)} newTab color="#000" hoverColor="#000">
+              {dependency}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <p>These packages contains the following license and notice below:</p>
+      <License>{license.content}</License>
+      <br />
+      <hr />
+      <br />
+    </Fragment>
+  );
+};
+
+const License = styled.p`
+  white-space: break-spaces;
+`;
+
+const getPackageUrl = (packageNameAndVersion: string) => {
+  const indexOfLastAt = packageNameAndVersion.lastIndexOf("@");
+  const packageName = packageNameAndVersion.substring(0, indexOfLastAt);
+  const packageVersion = packageNameAndVersion.substring(indexOfLastAt + 1);
+
+  return `https://www.npmjs.com/package/${packageName}/v/${packageVersion}`;
+};
